9d5db550faa97cd8fe52a9ab8db05693efd4768a
Yash Prasad <64632275+yashprasad234@users.noreply.github.com>
2024-04-09 12:41:01 +0530

    init

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const adminRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/admin", adminRouter)
+app.use("/user", userRouter)
+
+
+// Connect to MongoDB
+// DONT MISUSE THIS THANKYOU!!
+mongoose.connect('mongodb+srv://yashprasad272:magloo%4007@todo-app.ktz0jbz.mongodb.net/', { dbName: "courses" });
+
+app.listen(3000, () => console.log('Server running on port 3000'));
